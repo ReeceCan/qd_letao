f3ee7d17378cf7bcb40262b8ff1c38bc7156c85d
reeceCan <13959343263@163.com>
2018-09-26 23:49:24 +0800

    登录页面功能完成

--- public/back/js/login.js
+++ public/back/js/login.js
@@ -48,7 +48,7 @@ $(function () {
           },
           // 专门用于配置回调的函数说明
           callback: {
-            message: "用户名不存在"
+            message: "密码错误"
           }
 
         }
@@ -57,3 +57,67 @@ $(function () {
 
   });
 })
+
+
+
+
+$(function () {
+   /*
+    * 通过submit按钮提交表单 可以让表单校验插件进行校验  
+    * (1) 校验通过,默认提交表单 ->跳转页面
+    *   需要再校验通过后 阻止默认的提交 ,表单校验插件本身就会阻止默认的提交
+    * (2) 校验失败,阻止默认提交 
+    * 
+    *  思路: 注册表单校验成功事件, 阻止默认的表单提交, 通过 ajax 进行提交
+    **/
+
+  $('#form').on('success.form.bv',function (e) {
+    //  阻止表单默认事件  
+    e.preventDefault();
+    // 通过ajax进行提交
+    $.ajax({
+      type: "post",
+      url: "/employee/employeeLogin",
+      dataType: "json",
+      //通过表单序列化快速获取表单值
+      data: $('#form').serialize(),
+      success: function (info) {
+        // console.log(info);
+        if(info.success) {
+          // 登录成功,跳转到登录页  
+          location.href = "index.html";
+        }
+        //登录失败
+          //error 1000 用户名错误  1001 密码错误
+        if(info.error === 1000) {
+          // 用户名错误
+           // 将表单用户名校验状态从成功更新成失败, 并且给用户提示
+          $("#form").data("bootstrapValidator").updateStatus("username","INVALID","callback");
+
+        }
+        if(info.error === 1001) {
+          //密码错误 
+          $('#form').data("bootstrapValidator").updateStatus("password", "INVALID", "callback");
+        }
+                 
+      }
+    })
+  });
+
+
+});
+
+$(function () {
+  //  添加重置功能 
+  // validator.resetForm();//重置表单，并且会隐藏所有的错误提示和图标
+   //  调用插件的方法进行重置 
+    // 1. true, 表示将表单内容和校验状态都重置
+    // 2. false, 只重置校验状态
+    
+  $('[type="reset"]').click(function () {
+   
+    $('#form').data("bootstrapValidator").resetForm();
+  })
+
+
+});
