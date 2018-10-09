039a923f79840c863fb2e1a0426696a438344cc5
reeceCan <13959343263@163.com>
2018-10-09 20:43:23 +0800

    分类2图片上传bug

--- public/back/js/second.js
+++ public/back/js/second.js
@@ -50,8 +50,48 @@ $(function () {
     });
   }
 
+  // 功能3 点击添加分类按钮显示模态框 
+  $('#addSecond').click(function () {
+     $('#addModalSecond').modal('show');
+    // 点击一级分类,发送ajax请求,获取所有的一级分类列表.
+    $.ajax({
+      url: "/category/queryTopCategoryPaging",
+      type: "get",
+      data: {
+        page: 1,
+        pageSize: 100
+      },
+      dataType: "json",
+      success: function (info) {
+        //  console.log(info);
+        var htmlStr = template("dropdownTpl",info)
+        $('.dropdown-menu').html(htmlStr);
+      }
+    });
 
+    
+  
+  
+  })
 
+  // 给下拉列表中的a添加点击事件,获取a的文本设置给按钮 
+  $('.dropdown-menu').on('click','a',function () {
+    //  获取a的文本 
+    var txt = $(this).text();
+    // console.log(txt);
+    $('#dropdownTxt').text(txt);
+    
+  })
+  
 
+  // 图片上传初始化  
+  $('#fileupload').fileupload({
+    dataType:"json",
+    //e：事件对象
+    //data：图片上传后的对象，通过data.result.picAddr可以获取上传后的图片地址
+    done:function (e, data) {
+      console.log(data);
+    }
+});
 
 })
